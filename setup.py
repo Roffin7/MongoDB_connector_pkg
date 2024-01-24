9e047004b7e9e05576186651672064223824e943
Roffin7 <roffinjason@gmail.com>
2024-01-24 21:58:47 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+REPO_NAME = "MongoDB_connector_pkg"
+PKG_NAME= "Mongo_Connect"
+AUTHOR_USER_NAME = "Roffin7"
+AUTHOR_EMAIL = "roffinjason@gmail.com"
 
 setup(
     name=PKG_NAME,
